bc8da50ae24c04bcc81bc456d31fab2063f298c2
mattrauschenbach-cloud <mattrauschenbach@gmail.com>
2026-06-27 15:41:12 -0400

    Create Time.jsx

---
+++ src/pages/Time.jsx
@@ -0,0 +1,8 @@
+export default function Time() {
+  return (
+    <section className="panel">
+      <h3>Time Tracking</h3>
+      <p>Clock in, clock out, job selection, and daily hours will go here.</p>
+    </section>
+  );
+}
